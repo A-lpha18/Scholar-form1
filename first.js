edc27de91d60f501d4f603cb4484e8e9d05be574
A-lpha18 <163670986+A-lpha18@users.noreply.github.com>
2024-06-26 17:55:06 +0530

    Add index.html

---
+++ first.js
@@ -0,0 +1,14 @@
+console.log("Rohit Karmakar");
+alert("Fill the form");
+var a =prompt("What is your name?");
+alert("hola "+a);
+
+var blog=prompt("Write a post: ");
+var blogcount=blog.length;
+var sliceblog=blog.slice(0,200);
+alert("You have written "+sliceblog);
+
+
+
+
+
